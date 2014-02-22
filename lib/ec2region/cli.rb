a356a4a9a5f796252bfe34c866bbdd01ae177137
Taiki Sugawara <buzz.taiki@gmail.com>
2014-02-23 08:51:45 +0900

    add ec2region cli

---
+++ lib/ec2region/cli.rb
@@ -0,0 +1,13 @@
+require 'ec2region/fetcher'
+
+module EC2Region
+  class CLI
+    def run(args)
+      regions = EC2Region::Fetcher.open.regions
+      args.each do |ip|
+        name = regions.find(ip) || "Not Match"
+        puts "#{ip}: #{name}"
+      end
+    end
+  end
+end

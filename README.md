67c907436d60b533dc4a408079a78547bc62b143
Taiki Sugawara <buzz.taiki@gmail.com>
2014-02-23 10:05:47 +0900

    add usage

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Amazon EC2 Region Finder
+
+Simple Command line utility that describe Amazon EC2 region of specified IP.
+
+
+## Usage
+
+    $ ec2region <YOUR.EC2.IP.ADDRESS>
--- lib/ec2region/cli.rb
+++ lib/ec2region/cli.rb
@@ -1,14 +1,28 @@
+require 'optparse'
+
 module EC2Region
   # The ec2region cli
   class CLI
     # Run cli with args
-    # @param args [Array<String>] command line args
-    def run(args)
+    # @param progname [String] the program name
+    # @param args [Array<String>] the command line args
+    # @return [Integer] the result status
+    def run(progname, args)
+      opts = OptionParser.new
+      opts.banner = "Usage: #{progname} IP..."
+      args = opts.parse(args)
+      if args.empty?
+        $stderr.puts opts.help
+        return 1
+      end
+
       regions = PageParser.new(open(REGION_PAGE_URL)).regions
       args.each do |ip|
         name = regions.find(ip) || "Not Match"
         puts "#{ip}: #{name}"
       end
+
+      return 0
     end
   end
 end
